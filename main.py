212ca7f92c0550ce75857c4976fa3a075cb0386b
Min-Gyu Kim <45328809+KelpPickles@users.noreply.github.com>
2026-06-21 16:17:24 +0900

    fix: on_ready() 기준시 설정

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from datetime import datetime
+from zoneinfo import ZoneInfo
 import asyncio
 from services.openai_service import get_model
 
@@ -34,7 +35,7 @@ async def on_ready():
         channel = bot.get_channel(1518148680098648184)
 
         if channel is not None:
-            now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            now = datetime.now(ZoneInfo("Asia/Seoul")).strftime("%Y-%m-%d %H:%M:%S")
             await channel.send(f"@everyone\n"
                             f"**{bot.user}가 정상적으로 시작되었습니다.**\n"
                             f"배포 시간: `{now}`",
